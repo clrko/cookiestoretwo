16f29c9ee431399d2b1153c14331f73d9bb3e2af
Claire Kodia <claire.kodia@gmail.com>
2020-05-09 12:23:22 +0200

    Created a component addRecipes

---
+++ front/src/components/addRecipes.js
@@ -0,0 +1,18 @@
+import React from "react"
+import axios from "axios"
+
+class AddRecipes extends React.Component {
+    state = {
+        description: "",
+        ingredient: "",
+        name: "", 
+        tools: "",
+        success: false,
+    }
+
+    render() {
+        return <div></div>
+    }
+}
+
+export default AddRecipes
