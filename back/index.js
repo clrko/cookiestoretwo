ec831b48958993214fff1e8332cca21ae83e990b
Claire Kodia <claire.kodia@gmail.com>
2020-05-09 10:25:22 +0200

    Managed to get data front the back and to console.log it

--- back/index.js
+++ back/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
 const bodyParser = require("body-parser")
 const connection = require("./src/helper/db.js")
 const recipe = require("./src/routes/recipe.js")
@@ -9,6 +10,7 @@ const app = express()
 
 /* Middleware morgan pour voir l'activité du serveur au lancement d'une requete*/
 app.use(morgan("dev"))
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use("/recipe", recipe) /* si un user demande à aller dans /recipe, redirige vers le fichier recipe.js */
