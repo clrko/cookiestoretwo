435c20f326a2ed8f6d45602caefa98ae1f88a8ec
Claire Kodia <claire.kodia@gmail.com>
2020-05-09 12:33:49 +0200

    Coded a form in the addRecipes file

--- front/src/components/addRecipes.js
+++ front/src/components/addRecipes.js
@@ -10,8 +10,52 @@ class AddRecipes extends React.Component {
         success: false,
     }
 
+    handleSubmit = event => {
+        event.preventDefault()
+        console.log("affiche la state", this.state)
+    }
+
     render() {
-        return <div></div>
+        return (
+        <div className="recipeForm">
+            <form onSubmit={this.handleSubmit}>
+                <input 
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                />
+
+                <input 
+                    type="text"
+                    name="ingredient"
+                    placeholder="Ingredient"
+                    value={this.state.ingredient}
+                    onChange={this.handleChange}
+                />
+
+                <input 
+                    type="text"
+                    name="description"
+                    placeholder="Description"
+                    value={this.state.description}
+                    onChange={this.handleChange}
+                />
+
+                <input 
+                    type="text"
+                    name="tools"
+                    placeholder="Tools"
+                    value={this.state.tools}
+                    onChange={this.handleChange}
+                />
+
+                <input type="submit" value="Submit" />
+            </form>
+            {/* {this.state.success ? <p>ok</p> : null } */}
+        </div>
+        )
     }
 }
 
