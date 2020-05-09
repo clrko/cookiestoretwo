cce0e25e020a878ab27bee25bdca08d4adcbc019
Claire Kodia <claire.kodia@gmail.com>
2020-05-09 12:40:18 +0200

    Imported AddRecipes. Display ok

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AllRecipes from './components/allRecipes'
+import AddRecipes from './components/addRecipes'
 import './App.css';
 
 function App() {
   return (
     <div className="globalForm">
-      <AllRecipes />
+      {/* <AllRecipes /> */}
+      <AddRecipes />
     </div>
   );
 }
